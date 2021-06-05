13ccbb274d16c722d40271c4bec57f1cbcd88d07
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2021-06-05 21:57:30 +0200

    Project restructured, added screens. Major progress.

---
+++ assets/styles/textBox.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from "react-native";
+
+const textBoxStyles = StyleSheet.create({
+    textInputFocus:{
+        borderColor: '#19D4F8'
+    },
+    textInputBlurred:{
+        borderColor: '#2F80ED'
+    },
+})
+export default textBoxStyles;
---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import GradientButton from "react-native-gradient-buttons";
+import Icon from "react-native-vector-icons/Ionicons";
+import {Image, Platform, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
+import AppLoading from 'expo-app-loading';
+import {useFonts} from "expo-font";
+
+
+export default function WelcomeScreen() {
+
+}
