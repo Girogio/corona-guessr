a1ad0e87b8b6056c482d35aa803c3b404dddd90e
Girogio <giorgiogrigolo03@gmail.com>
2021-06-13 02:07:58 +0200

    + new achievement

--- assets/data/Achievements.js
+++ assets/data/Achievements.js
@@ -6,6 +6,13 @@ const Achievements = [
         description: 'Created a shiny new account.',
         iconProvider: 'ionicon',
         icon: 'checkmark-circle'
+    },
+    {
+        id: '2',
+        title: 'Master Guesser',
+        description: 'Guessed 3 consecutive days\' new cases.',
+        iconProvider: 'ionicon',
+        icon: 'checkmark-circle'
     }
 ]
 
