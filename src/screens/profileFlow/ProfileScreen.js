18fa0166683ca107f476f56ba822599bec593849
Girogio <giorgiogrigolo03@gmail.com>
2021-06-15 23:37:22 +0200

    added firebase upload

--- src/screens/profileFlow/ProfileScreen.js
+++ src/screens/profileFlow/ProfileScreen.js
@@ -25,7 +25,7 @@ import 'firebase/database'
 import 'firebase/storage'
 import Achievements from "../../../assets/data/Achievements";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import * as ImagePicker from 'expo-image-picker';
+import * as ImagePicker  from 'expo-image-picker';
 
 export default function ProfileScreen({navigation}) {
     const BadgedIcon = (MaterialCommunityIcons)
@@ -46,6 +46,7 @@ export default function ProfileScreen({navigation}) {
             mediaTypes: ImagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
             aspect: [1, 1],
+            base64: true,
             quality: 1,
         });
 
@@ -53,6 +54,25 @@ export default function ProfileScreen({navigation}) {
 
         if (!result.cancelled) {
             setImage(result.uri);
+// Implement a new Blob promise with XMLHTTPRequest
+            const blob: Blob = await new Promise((resolve, reject) => {
+                const xhr = new XMLHttpRequest();
+                xhr.onload = function () {
+                    resolve(xhr.response);
+                };
+                xhr.onerror = function () {
+                    reject(new TypeError("Network request failed"));
+                };
+                xhr.responseType = "blob";
+                xhr.open("GET", result.uri, true);
+                xhr.send(null);
+            });
+
+// Create a ref in Firebase (I'm using my user's ID)
+            const ref = firebase.storage().ref().child(`avatars/${user.uid}`);
+
+// Upload blob to Firebase
+            await ref.put(blob, {contentType: "image/png"});
         }
 
     }
